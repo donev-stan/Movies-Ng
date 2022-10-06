c9939034fd94d9600d8028ddbc16a71ea7b9c484
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-10-06 09:32:18 +0300

    initial commit

--- movie-app/src/app/app.component.spec.ts
+++ movie-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'movies-app'`, () => {
+  it(`should have as title 'movie-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('movies-app');
+    expect(app.title).toEqual('movie-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('movies-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('movie-app app is running!');
   });
 });
